f67552013cb997c717b0f30e47a7cb580a84f975
ReshadDev <reshadqarayev44@gmail.com>
2024-04-10 19:45:23 +0400

    type-system

---
+++ types.ts
@@ -0,0 +1,21 @@
+// Types
+
+// Hover ederek gore bilerik ki deyisken hansi tipdedir.
+
+/*
+
+What is type?
+- A shortcut to refer to the different properties and functions that a value has
+
+*/
+
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20,
+};
+
+class Color {}
+
+const red = new Color();
